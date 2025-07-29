2d34c6532aed8b271c9199199b387d1a5cab15b1
geraldbryan <geraldbryan9914@gmail.com>
2025-07-29 11:55:20 +0700

    change settings.py

--- periwatch_api/settings.py
+++ periwatch_api/settings.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'periwatch_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy'
     }
 }
 
