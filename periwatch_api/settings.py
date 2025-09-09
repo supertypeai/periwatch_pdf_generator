4739a8bfd6d706a33101aa6335cd926ec290249d
geraldbryan <geraldbryan9914@gmail.com>
2025-09-09 15:05:10 +0700

    add cors

--- periwatch_api/settings.py
+++ periwatch_api/settings.py
@@ -210,6 +210,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "http://localhost:5173",  # Vite default
     "http://127.0.0.1:5173",
+    "https://periwatch.com"
 ]
 
 # For development, allow all origins from localhost
